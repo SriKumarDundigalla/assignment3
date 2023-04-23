4a32cf54263b89e288f626a1c90f64b2ee07eb35
pk1akshay <pk1@usf.edu>
2023-04-23 10:55:33 -0400

    Updating all courses page and all spellings of other pages.

--- js/index.js
+++ js/index.js
@@ -2,20 +2,3 @@ function toggleDarkMode() {
     var body = document.getElementsByTagName("body")[0];
     body.classList.toggle("dark-mode");
 }
-// const starIcons = document.querySelectorAll('.rating');
-
-// starIcons.forEach((starIcon, index) => {
-//   starIcon.addEventListener('mouseover', () => {
-//     for (let i = 0; i <= index; i++) {
-//       starIcons[i].classList.remove('bi-star');
-//       starIcons[i].classList.add('bi-star-fill');
-//     }
-//   });
-
-//   starIcon.addEventListener('mouseout', () => {
-//     for (let i = 0; i <= index; i++) {
-//       starIcons[i].classList.remove('bi-star-fill');
-//       starIcons[i].classList.add('bi-star');
-//     }
-//   });
-// });
